b1bbc61e07d8834d626f645d9e07171aa6d3ed61
celi21 <134336682+celi21@users.noreply.github.com>
2024-01-15 04:01:06 -0500

    new83change

---
+++ client/src/utils/constants.js
@@ -0,0 +1,25 @@
+export const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const cities = [
+  "New York",
+  "Pakistan",
+  "India",
+  "Rochester",
+  "Delhi",
+  "baby",
+];
+
+export const filters = [
+ "Pretty",
+ "Wi-Fi",
+];
+
+export const sorters = ["Price"];
